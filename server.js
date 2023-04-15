b57e4b914cffb5f1f1460075cc5af5fe01ca17ff
Matija Jeras <matija.jeras64@gmail.com>
2023-04-15 02:40:41 +0200

    ffmpeg, users and other fixes

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ const upload = multer(storage);
 
 //Configure YoutubeMp3Downloader with your settings
 var YD = new YoutubeMp3Downloader({
-    "ffmpegPath": "./ffmpeg.exe",        // FFmpeg binary location
+    "ffmpegPath": "./ffmpeg",        // FFmpeg binary location
     "outputPath": "./songs",    // Output file location (default: the home directory)
     "youtubeVideoQuality": "highestaudio",  // Desired video quality (default: highestaudio)
     "queueParallelism": 2,                  // Download parallelism (default: 1)
@@ -62,28 +62,44 @@ var YD = new YoutubeMp3Downloader({
 app.use(express.static(__dirname + '/public'));
 
 app.use(cors({
-    origin: 'http://localhost:3001'
+    origin: [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'https://muzikk.vercel.app'
+    ],
 }));
 
+app.get('/', (req, res) => {
+    res.json({ response: 'Success' })
+});
+
 // Token auth middleware
-app.use((req, res, next) => {
-    // console.log('Time:', Date.now())
-    if (!req.headers || !req.headers.authentication || req.headers.authentication !== config.AUTH_TOKEN) {
-        return res.status(401).json({ error: 'UNAUTHORIZED'});
-    }
-    next();
-})
+// app.use((req, res, next) => {
+//     // console.log('Time:', Date.now())
+//     if (!req.headers || !req.headers.authentication || req.headers.authentication !== config.AUTH_TOKEN) {
+//         return res.status(401).json({ error: 'UNAUTHORIZED'});
+//     }
+//     next();
+// })
 
 // Define route to stream audio files
 app.get('/song/:filename', (req, res) => {
     // Read audio file from disk
-    const audioFile = fs.createReadStream(`./songs/${req.params.filename}`);
+
+    const filePath = `./songs/${req.params.filename}`;
+    const stat = fs.statSync(filePath);
+    const audioFile = fs.createReadStream(filePath);
 
     // Create a lame encoder to convert audio to MP3
     // const encoder = new Lame({
     //     output: 'mp3',
     //     bitrate: 128,
     // });
+
+    res.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Content-Length': stat.size
+    });
 
     // Pipe the audio file to the encoder, and the encoder to the response
     audioFile.pipe(res);
@@ -146,6 +162,21 @@ app.get('/yt-search', (req, res) => {
     })
 });
 
+app.post('/user-to-song', (req, res) => {
+
+    const id = req.body.id;
+    const user = req.body.user;
+
+    const { songs } = db.data;
+    const songToUpdate = songs.find(song => song.id === id);
+    if (songToUpdate) {
+        songToUpdate.users = [...songToUpdate.users, user];
+        db.write();
+        return res.json({ message: 'Added successfully' })
+    }
+    return res.json({ error: `Error: song with id ${id} not found` })
+});
+
 app.post('/yt-add', (req, res) => {
 
     const id = req.body.id;
@@ -155,30 +186,50 @@ app.post('/yt-add', (req, res) => {
     const timestamp = req.body.timestamp;
     const artist = req.body.artist;
     const title = req.body.title;
+    const releaseYear = req.body.releaseYear;
+    const user = req.body.user;
 
-    const { songs } = db.data;
-    songs.push({
-        id,
-        file: `${artist} - ${title}.mp3`,
-        title,
-        artist,
-        duration,
-        timestamp,
-        image,
-        createdAt: new Date().toISOString(),
-    });
-    db.write();
-
-    YD.on("finished", function(err, data) {
-        try {
-            res.json({ message: 'Successfully added to library' })
-        } catch (e) {
-            console.error(e)
-        }
-    });
-    YD.download(id, `${artist} - ${title}.mp3`);
+    try {    
+        YD.on("finished", function(err, data) {
+            try {
+                const { songs } = db.data;
+                songs.push({
+                    id,
+                    file: `${artist} - ${title}.mp3`,
+                    title,
+                    artist,
+                    duration,
+                    timestamp,
+                    image,
+                    releaseYear,
+                    users: [user],
+                    createdAt: new Date().toISOString(),
+                });
+                db.write();
+                res.json({ message: 'Successfully added to library' })
+            } catch (e) {
+                console.log('ERROR adding song - wrting to db: ');
+                console.error(e)
+            }
+        });
+        YD.on("error", function(error) {
+            console.log('YTD Error: ');
+            console.log(error);
+            res.json({ error: 'YTD error' })
+        });
+        YD.download(id, `${artist} - ${title}.mp3`);
+    } catch (e) {
+        console.log('ERROR adding song: ');
+        console.log(e);
+    }    
 });
 
+// Change DB
+// const { songs } = db.data;
+// songs.forEach((song) => {
+//   song.users = ['matija'];
+// });
+// db.write();
 
 // Start server listening on port 3000
 server.listen(3000, () => {
