726ef9fb4af6d7d1481a85a7deaad59fffbc5d8c
Matija Jeras <matija.jeras64@gmail.com>
2023-04-20 17:16:21 +0200

    fix port

--- server.js
+++ server.js
@@ -294,7 +294,6 @@ app.post('/yt-add', (req, res) => {
 // });
 // db.write();
 
-// Start server listening on port 3000
-server.listen(3000, () => {
-    console.log('Server listening on port 3000');
+server.listen(process.env.PORT || 3000, () => {
+    console.log(`Server listening on port ${process.env.PORT || 3000}`);
 });
